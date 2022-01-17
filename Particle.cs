da54c1e90c53091b820d7f60abe1e2a940bdfd38
Ludmila Yegupova <yegupova.mila@bk.ru>
2022-01-17 22:01:59 +0800

    Добавьте файлы проекта.

---
+++ Particle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egupova.kursovic
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+
+        // добавили генератор случайных чисел
+        public static Random rand = new Random();
+
+        public Particle()
+        {
+            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+        }
+    }
+    // конструктор по умолчанию будет создавать кастомную частицу
+    
+}

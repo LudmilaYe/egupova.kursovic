bb2f13be3618f5df5b24728ec703e11b47de1f25
Ludmila Yegupova <yegupova.mila@bk.ru>
2022-01-18 02:00:02 +0800

    Создаем а-ля здоровья

--- Particle.cs
+++ Particle.cs
@@ -14,7 +14,7 @@ namespace egupova.kursovic
         public float Y; // Y координата положения частицы в пространстве
         public float Direction; // направление движения
         public float Speed; // скорость перемещения
-
+        public float Life;//запас здоровья частицы 
         // добавили генератор случайных чисел
         public static Random rand = new Random();
 
@@ -25,11 +25,12 @@ namespace egupova.kursovic
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
             Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);//Добавили исходный запас здоровья от 20 до 120
         }
         public void Draw(Graphics g)
         {
             // создали кисть для рисования
-            var b = new SolidBrush(Color.Black);
+            var b = new SolidBrush(Color.Blue);
 
             // нарисовали залитый кружок радиусом Radius с центром в X, Y
             g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
